3abb1b07cee308508c6f4c4057394691045b1af0
Basile REGNEAU <bregneau@student.42.fr>
2024-04-22 14:41:01 +0200

    ex03: 	fixed display of the ast

--- ex03/AstNode.cpp
+++ ex03/AstNode.cpp
@@ -1,5 +1,6 @@
 #include "AstNode.hpp"
 #include <iostream>
+#include <algorithm>
 
 Negation::Negation(AstNodePtr node)
 : _node(std::move(node))
@@ -27,38 +28,11 @@ BinaryOperator::BinaryOperator(char op, AstNodePtr left, AstNodePtr right)
 {
 }
 
-std::vector<std::string> BinaryOperator::to_strings() const
+static std::vector<std::string> merge_strings(const std::vector<std::string> &left, const std::vector<std::string> &right)
 {
-	std::vector<std::string> left;
-	std::vector<std::string> right;
 	std::vector<std::string> result;
-
-	const std::vector<std::string> left_strings = _left->to_strings();
-	const std::vector<std::string> right_strings = _right->to_strings();
-	size_t left_size = left_strings[0].size();
-	// std::cout << "left_size = " << left_size << std::endl;
-	// for (const std::string &l: left_strings)
-	// 	std::cout << l << std::endl;
-	size_t right_size = right_strings[0].size();
-	// std::cout << "right_size = " << right_size << std::endl;
-	// for (const std::string &r: right_strings)
-	// 	std::cout << r << std::endl;
-
-	std::string spaces(left_strings.size(), ' ');
-	// std::cerr << "spaces.size() = " << spaces.size() << std::endl;
-	for (size_t i = 0; i < left_strings.size(); i++)
-		left.push_back(spaces + std::string(left_size - i, ' ') + '/' + std::string(i, ' '));
-	for (const std::string &l: left_strings)
-		left.push_back(" " + l + " ");
-	spaces = std::string(right_strings.size(), ' ');
-	for (size_t i = 0; i < right_strings.size(); i++)
-		right.push_back(std::string(i, ' ') + "\\" + std::string(right_size - i, ' ') + spaces);
-	for (const std::string &r: right_strings)
-		right.push_back(" " + r + " ");
 	size_t size_max = std::max(left.size(), right.size());
-
-	result.push_back(std::string(left.size()+1, ' ') + op + std::string(right.size()+1, ' '));
-	for (size_t i = 0; i <= size_max; i++)
+	for (size_t i = 0; i < size_max; i++)
 	{
 		std::string line;
 		if (i < left.size())
@@ -72,6 +46,34 @@ std::vector<std::string> BinaryOperator::to_strings() const
 			line += std::string(right[0].size(), ' ');
 		result.push_back(line);
 	}
+	return result;
+}
+
+std::vector<std::string> BinaryOperator::to_strings() const
+{
+	std::vector<std::string> left;
+	std::vector<std::string> right;
+	std::vector<std::string> result;
+
+	const std::vector<std::string> left_strings = _left->to_strings();
+	const std::vector<std::string> right_strings = _right->to_strings();
+	size_t left_size = left_strings[0].size() / 2;
+	size_t right_size = std::find_if_not(right_strings[0].begin(), right_strings[0].end(), isspace) - right_strings[0].begin();
+
+
+	std::string spaces(left_strings.size(), ' ');
+	// std::cerr << "spaces.size() = " << spaces.size() << std::endl;
+	for (size_t i = 0; i <= left_size; i++)
+		left.push_back(spaces + std::string(left_size - i, ' ') + '/' + std::string(i, ' '));
+	for (const std::string &l: left_strings)
+		left.push_back(l + " ");
+	right_size = std::find_if_not(right_strings[0].begin(), right_strings[0].end(), isspace) - right_strings[0].begin();
+	spaces = std::string(right_strings.size(), ' ');
+	for (size_t i = 0; i <= right_size; i++)
+		right.push_back(std::string(i, ' ') + "\\" + std::string(right_size - i, ' ') + spaces);
+	for (const std::string &r: right_strings)
+		right.push_back(" " + r);
+	result = merge_strings(left, right);
 
 	return result;
 }
